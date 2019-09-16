60f327eb0167b2acb58c7e537bee20ac247333f9
Anthony Tranduc <anthonytranduc@gmail.com>
2019-09-16 19:46:06 -0400

    started on views

--- basic-poll/polls/admin.py
+++ basic-poll/polls/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Question,Choice
 
-# Register your models here.
+admin.site.site_header = "Poll App Admin"
+admin.site.site_title = "Poll App Admin Area"
+admin.site.index_title = "Welcome to the Poll App Admin Area"
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                ('Date Information', {'fields': ['pub_date'], 'classes': ['collaspe']}),]
+    inlines = [ChoiceInLine]
+
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choice)
